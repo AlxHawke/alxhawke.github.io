b14c76966fe3577eaa800014c03afa5a1ce5bf41
AlxHawke <109279190+AlxHawke@users.noreply.github.com>
2022-12-13 07:56:05 +0300

    Delete practic_9 directory

--- practic_9/js/main.js
+++
@@ -1,8 +0,0 @@
-const priceLink = document.querySelectorAll('.price-link');
-const priceList = document.querySelectorAll('.price-list-box')
-
-for (let i=0; i<priceLink.length; i++) {
-	priceLink[i].addEventListener('click', () => {
-		priceList[i].classList.toggle('active');
-	})
-}
